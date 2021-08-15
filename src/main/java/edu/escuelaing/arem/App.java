14c4120607187d85f706e83dfb26d7a7a7adf224
J-382 <jrperezdeleon@gmail.com>
2021-08-15 16:37:32 -0500

    App test

--- src/main/java/edu/escuelaing/arem/App.java
+++ src/main/java/edu/escuelaing/arem/App.java
@@ -1,13 +1,11 @@
 package edu.escuelaing.arem;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import static spark.Spark.*;
+
+public class App {
+    public static void main(String[] args) {
+        get("/", (req, res) -> "Working! ma nigga");
+        get("/hello", (req, res) -> "Hello World!");
     }
+    
 }
