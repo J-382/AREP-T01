bc2af39e5b92d973a68224d0935f4572b7fbd29c
J-382 <jrperezdeleon@gmail.com>
2021-08-16 17:54:13 -0500

    Heroku first commit

--- src/main/java/edu/escuelaing/arep/App.java
+++ src/main/java/edu/escuelaing/arep/App.java
@@ -1,15 +1,15 @@
-package edu.escuelaing.arem;
+package edu.escuelaing.arep;
 
 import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args) {
         get("/", (req, res) -> "Working! ma nigga");
-        get("/hello", (req, res) -> ""+System.getenv("PORT"));
+        get("/hello", (req, res) -> "Hello World!");
     }
-    public static int getPort() {
+    
+    static int getPort() {
         if (System.getenv("PORT") != null) {
-            System.out.println(System.getenv("PORT"));
             return Integer.parseInt(System.getenv("PORT"));
         }
         return 4567; //returns default port if heroku-port isn't set(i.e. on localhost)
