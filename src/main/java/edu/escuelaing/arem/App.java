accb3ce442732b220dc43138a7dc4529994bc1fd
J-382 <jrperezdeleon@gmail.com>
2021-08-16 16:50:27 -0500

    Fixeds for heroku

--- src/main/java/edu/escuelaing/arem/App.java
+++ src/main/java/edu/escuelaing/arem/App.java
@@ -7,5 +7,10 @@ public class App {
         get("/", (req, res) -> "Working! ma nigga");
         get("/hello", (req, res) -> "Hello World!");
     }
-    
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567; //returns default port if heroku-port isn't set(i.e. on localhost)
+    }
 }
